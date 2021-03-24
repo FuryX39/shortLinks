75b374f7180d2c7c8e9d8630155b7cd8635198fd
FuryX39 <58467715+FuryX39@users.noreply.github.com>
2021-03-24 20:09:42 +0300

    Created DB and simple render

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+
+    full_link = models.CharField(max_length=511, verbose_name='Full link')
+    short_id = models.CharField(max_length=15, verbose_name='Short ID')
+    count = models.PositiveIntegerField(default=0, verbose_name='Redirect counter')
+
+    def __str__(self):
+        return self.short_id
