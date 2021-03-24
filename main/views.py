5e3c1930e0f2713de968d83362479b008d7a2dce
FuryX39 <58467715+FuryX39@users.noreply.github.com>
2021-03-25 01:59:55 +0300

    Updated views.index

--- main/views.py
+++ main/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.utils.crypto import get_random_string
+
+from .forms import LinkForm
+from .models import Link
 
 
 def index(request):
-    return render(request, 'index.html')
+
+    short_id = ''
+    form = LinkForm()
+    if request.method == 'POST':
+        short_id = create_short_id()
+        Link.objects.create(full_link=request.POST.get('full_link'), short_id=short_id)
+
+    return render(request, 'index.html', {'form': form, 'short_id': short_id})
+
+
+def create_short_id():
+
+    short_id_len = 6
+    while True:
+        short_id = get_random_string(short_id_len)
+        if not(Link.objects.filter(short_id=short_id)):
+            return short_id
