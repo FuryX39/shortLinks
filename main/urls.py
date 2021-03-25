9fc3a041054ec7e33f135b23fbc5a706da6629dd
FuryX39 <58467715+FuryX39@users.noreply.github.com>
2021-03-25 03:33:19 +0300

    Finished

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, redirect_to_full
 
 
 urlpatterns = [
-    path('', index, name='Home')
+    path('', index, name='Home'),
+    path('<str:short_id>/', redirect_to_full)
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
 from django.utils.crypto import get_random_string
 
 from .forms import LinkForm
@@ -10,11 +11,12 @@ def index(request):
     short_id = ''
     form = LinkForm()
     if request.method == 'POST':
-        if Link.objects.filter(full_link=request.POST.get('full_link')):
-            short_id = Link.objects.filter(full_link=request.POST.get('full_link')).first().short_id
+        full_link = f'http://{request.POST.get("full_link")}'
+        if Link.objects.filter(full_link=full_link):
+            short_id = Link.objects.filter(full_link=full_link).first().short_id
         else:
             short_id = create_short_id()
-            Link.objects.create(full_link=request.POST.get('full_link'), short_id=short_id)
+            Link.objects.create(full_link=full_link, short_id=short_id)
 
     return render(request, 'index.html', {'form': form, 'short_id': short_id})
 
@@ -26,3 +28,11 @@ def create_short_id():
         short_id = get_random_string(short_id_len)
         if not(Link.objects.filter(short_id=short_id)):
             return short_id
+
+
+def redirect_to_full(request, short_id):
+
+    try:
+        return redirect(Link.objects.filter(short_id=short_id).first().full_link)
+    except AttributeError:
+        return HttpResponse('')
